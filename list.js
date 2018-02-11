53e1cc942f33f4cea90ba6b2213a7ec2b57f05f5
adrian.nitu <adrian.nitu,92@gmail.com>
2018-02-11 15:56:03 +0200

    Refactor canTakeCard

--- list.js
+++ list.js
@@ -21,7 +21,7 @@ var List = function(name, start, end, duration, updateFrequency, previous, next,
 	this.mitCount = 0;
 	this.maxMitCount = maxMitCount;
 	this.percGap = 0.1; // 10% default free
-	this.flagGap = 2;   // 2 hours free, by default
+	this.flatGap = 2;   // 2 hours free, by default
 
 	graph.initCount(this.name, this.ticks);
 
@@ -32,44 +32,6 @@ var List = function(name, start, end, duration, updateFrequency, previous, next,
 			   (this.updateFrequency === ONCE_A_WEEK && onceAWeekFlag);
 	}
 
-	this.canTakeCard = function(card) {
-		var percGap = this.percGap;
-		var flatGap = this.flatGap;
-		var mitGap  = (this.maxMitCount * this.percGap);
-		var cardTick = 0.125;
-
-		// card can be undefined for a generic test
-		if(card !== undefined){
-			// don't move more than 1 list away
-			if(card.date > lists[card.listname].end) {
-				return false;
-			}
-
-			if(card.date < this.end) {
-				// if the task must be done now, the gap should be 0
-				percGap = 0.0;
-				flatGap = 0;
-				mitGap  = 0;
-			}
-			cardTick = card.tick;
-		}
-		if (this.sumTicks + flatGap + cardTick >= this.ticks){
-			return false;
-		}
-
-		if (this.sumTicks + cardTick > (this.ticks * (1-percGap))) {
-			return false;
-		}
-		if (this.cardCount + 1 > (this.maxCount * (1-percGap))){
-			return false
-		}
-		if (this.mit &&
-			this.mitCount + 1 > (this.mitCount - mitGap)) {
-			return false;
-		}
-
-		return true;
-	}
 	this.takeCardFrom = function(card, list) {
 		reqCounter = reqCounter +1;
 		this.sumTicks += card.tick;
@@ -117,6 +79,55 @@ var List = function(name, start, end, duration, updateFrequency, previous, next,
 		 }, (reqCounter - 1) * 250);
 		}
 
+	this.canTakeCard = function(card) {
+		var percGap = this.percGap;
+		var flatGap = this.flatGap;
+		var mitGap  = (this.maxMitCount * this.percGap);
+		var cardTick = 0.125;
+
+		// card can be undefined for a generic test
+		if(card !== undefined){
+			// don't move more than 1 list away
+			var clist = lists[card.listname];
+			var nnclist = lists[clist.next];
+			if(nnclist !== undefined && card.date > nnclist.end) {
+				return false;
+			}
+
+			if(card.date <= this.end) {
+				// if the task must be done now, the gaps should be 0
+				percGap = 0.0;
+				flatGap = 0;
+				mitGap  = 0;
+			}
+			cardTick = card.tick;
+
+			if (card.mit &&
+				this.mitCount + 1 > (this.mitCount - mitGap)) {
+				return false;
+			}
+		}
+		if (this.sumTicks + flatGap + cardTick > this.ticks){
+			return false;
+		}
+
+		if (this.sumTicks + cardTick > (this.ticks * (1-percGap))) {
+			return false;
+		}
+
+		if (this.cardCount + 1 > (this.maxCount * (1-percGap))){
+			return false
+		}
+
+		if(card !== undefined) {
+				console.log(card.name + " can fit into " + this.name);
+				console.log(this.sumTicks + "+" + flatGap + "+" + cardTick + "<" + this.ticks);
+				console.log(this.sumTicks + "+" + cardTick + "<" + (this.ticks * (1-percGap)));
+				console.log("=======================");
+			}
+		return true;
+	}
+
 	this.reasign_card_to_proper_list = function() {
 		var prevListName = this.previous;
 		if(prevListName === null) {
@@ -127,7 +138,6 @@ var List = function(name, start, end, duration, updateFrequency, previous, next,
 		var nnlist = lists[this.next];
 		var targetListId = prevList.id;
 		var inbox = lists["Inbox"];
-		var quickExit = false;
 		var cards = this.cards;
 		for(var c in cards){
 			var card = cards[c];
@@ -141,34 +151,36 @@ var List = function(name, start, end, duration, updateFrequency, previous, next,
 						return false;
 					}
 					inbox.takeCardFrom(card, this);
-					quickExit = true;
 				}
 			}
 			else {
 				if(nnlist !== undefined && due > nnlist.end){
 					nnlist.takeCardFrom(card, this);
+					continue;
 				}
-				if(nnlist !== undefined && due > this.end &&
-					this.sumTicks - card.tick > (this.ticks * (1-this.gap))){
-					nnlist.takeCardFrom(card, this);
-				}
-				if(nnlist !== undefined && due > this.end &&
-					this.cardCount - 1 > (this.maxCount * (1-this.gap))){
-					nnlist.takeCardFrom(card, this);
-				}
-				if(nnlist !== undefined && due > this.end &&
-					card.mit &&
-					this.mitCount - 1 > (this.maxMitCount * (1-this.gap))){
-					nnlist.takeCardFrom(card, this);
+				if(nnlist !== undefined && due > this.end){
+					if(this.sumTicks - card.tick > this.ticks - this.flatGap){
+						nnlist.takeCardFrom(card, this);
+						continue;
+					}
+					if(this.sumTicks - card.tick > (this.ticks * (1-this.percGap))){
+						nnlist.takeCardFrom(card, this);
+						continue;
+					}
+					if(this.cardCount - 1 > (this.maxCount * (1-this.percGap))){
+						nnlist.takeCardFrom(card, this);
+						continue;
+					}
+					if(card.mit &&
+						this.mitCount - 1 > (this.maxMitCount * (1-this.percGap))){
+						nnlist.takeCardFrom(card, this);
+						continue;
+					}
 				}
 				if(flag && card.tick > prevList.ticks/4){
 					addWarning("I need you to split <b>" + card.name + "</b><br/>");
 				}
 			}
-		}
-		if(quickExit) {
-			console.log(this.name + " is full");
-			return true;
 		}
 		if(prevList.canTakeCard()){
 			for(var c in cards) {
