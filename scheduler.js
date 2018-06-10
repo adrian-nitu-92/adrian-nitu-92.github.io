2af3c60497fe53e83989a6da5ec7afa85f000e2e
adrian <adrian.nitu.92@gmail.com>
2018-06-10 11:21:03 +0300

    Refactor scheduler

--- scheduler.js
+++ scheduler.js
@@ -2,6 +2,22 @@
 
 var Scheduler = function () {
 
-   this.done = false;
+	this.done = false;
+
+	/* init */
+	justReset = (localStorage.getItem("reset") === "undefined");
+
+	localStorage.setItem("reset", "value");
+
+	Trello.authorize({
+		type: 'popup',
+		name: 'Getting Started Application',
+		scope: {
+			read: 'true',
+			write: 'true' },
+			expiration: 'never',
+			success: every5,
+			error: console.log
+		});
 }
 
